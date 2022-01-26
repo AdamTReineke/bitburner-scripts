f3b7734456197441c144dc1cd2c6d056b5ab0ca8
Adam Reineke <adam@reineke.me>
2022-01-25 20:14:34 -0800

    add verbose mode to market

--- market.js
+++ market.js
@@ -1,39 +1,27 @@
-import { format, formatTime, formatPercent, pad } from "lib.js";
-/** @type {NS} */
-var ns;
-
-let details = [];
+import { format, formatPercent, pad } from "lib.js";
 
 /** @param {NS} ns **/
-export async function main(NS) {
-	ns = NS;
-	const trade = ns.args[0] === "trade";
+export async function main(ns) {
+	const trade = ns.args.includes("trade");
+	const verbose = ns.args.includes("verbose");
 
 	// Get list of symbols - Shouldn't change
 	const symbols = ns.stock.getSymbols();
 
-	// Load existing data, if it exists
-	if(ns.fileExists("stock.txt")) {
-		details = JSON.parse(ns.read("stock.txt"));
-	}
-	// Or get initial round of data
-	else {
-		details = symbols.map((symbol) => {
-			return {
-				symbol,
-				forecast: ns.stock.getForecast(symbol),
-				min: ns.stock.getPrice(symbol),
-				max: ns.stock.getPrice(symbol),
-				price: ns.stock.getPrice(symbol),
-			}
-		});
-	}
+	/** @type {Array<Stock>} */
+	let details = symbols.map((symbol) => {
+		return {
+			symbol,
+			forecast: ns.stock.getForecast(symbol),
+			min: ns.stock.getPrice(symbol),
+			max: ns.stock.getPrice(symbol),
+			price: ns.stock.getPrice(symbol),
+			sharesLong: 0,
+			sharesShort: 0,
+		}
+	});
 
 	while(true) {
-		// print details
-		table(details);
-		await ns.write("stock.txt", JSON.stringify(details), "w");
-		await ns.sleep(6000);
 		ns.clearLog();
 
 		// update
@@ -53,7 +41,7 @@ export async function main(NS) {
 					var settled = ns.stock.sell(stock.symbol, position[0]);
 					if(settled > 0) {
 						
-						ns.tprintf(`${new Date().toLocaleTimeString()} SOLD: ${stock.symbol} x ${format(position[0])} @ \$ ${format(settled)} = \$ ${format(settled * position[0])}, profitting \$ ${format(settled * position[0] - 200_000 - position[1] * position[0])}`);
+						ns.print(`${new Date().toLocaleTimeString()} SOLD: ${stock.symbol} x ${format(position[0])} @ \$ ${format(settled)} = \$ ${format(settled * position[0])}, profitting \$ ${format(settled * position[0] - 200_000 - position[1] * position[0])}`);
 					}
 					return;
 				}
@@ -63,7 +51,7 @@ export async function main(NS) {
 					var price = ns.stock.buy(stock.symbol, Math.min(toBuy, ns.stock.getMaxShares(stock.symbol)));
 					var boughtQty = ns.stock.getPosition(stock.symbol)[0];
 					if(boughtQty > 0) {
-						ns.tprintf(`${new Date().toLocaleTimeString()} BOUGHT ${stock.symbol} x ${format(boughtQty)} @ ${format(price)} = \$ ${format(price * boughtQty)}`);
+						ns.print(`${new Date().toLocaleTimeString()} BOUGHT ${stock.symbol} x ${format(boughtQty)} @ ${format(price)} = \$ ${format(price * boughtQty)}`);
 					}
 					return;
 				}
@@ -72,28 +60,27 @@ export async function main(NS) {
 		});
 
 		// print details
-		table(details);
-		await ns.write("stock.txt", JSON.stringify(details), "w");
+		table(ns, details, verbose);
 		await ns.sleep(6000);
 		ns.clearLog();
-		
 	}
 }
 
 /**
- * @param {Array<{symbol: string}>} details
+ * @param {NS} ns
+ * @param {Stock[]} details
+ * @param {boolean} verbose
  */
-function table(details) {
-	ns.print(
-		pad("Symbol", 8),
-		pad("Price", 11),
-		pad("Forecast", 10),
-		pad("Long Pct", 10),
-		pad("Short Pct", 10)
-	);
-
-	// alpha sort
-	//details.sort((a, b) => a.symbol.localeCompare(b.symbol));
+function table(ns, details, verbose) {
+	if(verbose) {
+		ns.print(
+			pad("Symbol", 8),
+			pad("Price", 11),
+			pad("Forecast", 10),
+			pad("Long Pct", 10),
+			pad("Short Pct", 10)
+		);
+	}
 
 	// upside sort
 	details.sort((a, b) => b.forecast - a.forecast );
@@ -109,31 +96,19 @@ function table(details) {
 		var posInd = ns.stock.getPosition(item.symbol)[0] > 0 ? "+ " :
 			ns.stock.getPosition(item.symbol)[2] > 0 ? "- " : "  ";
 
-		ns.print(
-			pad(posInd + item.symbol, 8),
-			pad(format(item.price), 11),
-			pad(formatPercent(item.forecast), 10),
-			pad(formatPercent(item.sharesLong / ns.stock.getMaxShares(item.symbol)), 10),
-			pad(formatPercent(item.sharesShort / ns.stock.getMaxShares(item.symbol)), 10)
-		);
+		if(verbose) {
+			ns.print(
+				pad(posInd + item.symbol, 8),
+				pad(format(item.price), 11),
+				pad(formatPercent(item.forecast), 10),
+				pad(formatPercent(item.sharesLong / ns.stock.getMaxShares(item.symbol)), 10),
+				pad(formatPercent(item.sharesShort / ns.stock.getMaxShares(item.symbol)), 10)
+			);
+		}
 	});
 
 	ns.print(`  Long Positions: \$ ${format(sum)}`);
 	ns.print(`      Cost Basis: \$ ${format(costBasis)}`);
 	ns.print(`Unrealized Gains: \$ ${format(sum - costBasis)}`);
 	ns.print(`    Total Market: \$ ${format(totalMarket)}`)
-}
-
-/**
- * @param {{ price: number, min: number, max: number}} stock
- */
-function getRange(stock) {
-	var range = stock.max - stock.min;
-	var cur = stock.price - stock.min;
-
-	var pct = (cur / range).toFixed(1) * 10;
-	var str = "...........".split("");
-	str[pct] = "$";
-
-	return str.join("");
 }
---
+++ stock.d.ts
@@ -0,0 +1,9 @@
+interface Stock {
+    symbol: string;
+    forecast: number;
+    min: number;
+    max: number;
+    price: number;
+    sharesLong: number;
+    sharesShort: number;
+}
