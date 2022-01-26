cd8be6ee0b541a4d35acb079ed93798a4847548f
Adam Reineke <adam@reineke.me>
2022-01-25 19:57:19 -0800

    move market and server later

--- endgame.js
+++ endgame.js
@@ -9,9 +9,6 @@ export async function main(NS) {
     ns = NS;
 	ns.disableLog("sleep");
 
-	ns.print(`STAGE: -`);
-	ns.rm("stock.txt", "home");
-	ns.exec("market.js", "home", 1);
 	await trainAsync('hack', 100);
 	await deploy();
 
@@ -35,7 +32,9 @@ export async function main(NS) {
 	ns.joinFaction("Daedalus");
 	ns.workForFaction("Daedalus", "Hacking contracts", false);
 
-	await buyServers("D", 2**20);
+	ns.exec("server-upgrader.js", "home", 1, "100", "B");
+
+	ns.exec("market.js", "home", 1, "trade");
 
 	/*
 	while(ns.getFactionRep("Daedalus") < 2_500_000) {
