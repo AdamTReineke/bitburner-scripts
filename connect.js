6fdb8163666a216036f8dbd5990b838e1253d264
Adam Reineke <adam@reineke.me>
2022-02-09 18:31:25 -0800

    refactor connect to separate logic

--- connect.js
+++ connect.js
@@ -1,63 +1,91 @@
-import { format, formatTime, formatPercent, pad } from "lib.js";
-
-/** @type {NS} ns */
-var ns;
-
 /** @param {NS} ns **/
-export async function main(NS) {
-	ns = NS;
-	var scannedNames = new Set();
+export async function main(ns) {
+	var { scannedNames, scannedFromDict } = getServerNames(ns);
+	/**
+	 * @type {string}
+	 * Set the starting node to the first arg. Type coerce and avoid undefined arg.
+	 */
+	var start = "" + (ns.args[0] || "");
+
+	var matches = scannedNames.filter(v => v.startsWith(start.toLocaleUpperCase()) || v.startsWith(start.toLocaleLowerCase()));
+
+	
+	if (matches.length > 1) {
+		ns.tprintf(`Found ${matches.length}:`);
+		ns.tprintf(matches
+			.filter(host => !host.startsWith("AUTO-"))
+			.join(", ")
+		);
+	}
+
+
+	if (matches.length === 1) {
+		ns.tprintf("One match found... connecting directly.");
+
+		// Walk the servers
+		getPathToServer(scannedFromDict, matches[0]).forEach((host) => {
+			ns.connect(host);
+		});
+	}
+	
+	if (matches.length === 0) {
+		ns.tprintf(`List of all servers:`);
+		ns.tprintf(scannedNames
+			.filter(host => !host.startsWith("AUTO-"))
+			.sort((a, b) => a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()))
+			.join(", ")
+		);
+	}
+}
+
+/**
+ * 
+ * @param {NS} ns 
+ * @returns {{ scannedNames: string[], scannedFromDict: Map<string, string>}}
+ */
+function getServerNames(ns) {
+	var scannedNamesSet = new Set();
+	/**
+	 * @type {Set<string>} All server names, as strings.
+	 */
 	var allNames = new Set();
-	var scannedFromDict = {};
+	/**
+	 * @type {Map<string, string>}
+	 */
+	var scannedFromMap = new Map();
 	allNames.add("home");
 
 	var iter = allNames.values();
-	while(allNames.size > scannedNames.size) {
-		var toScan = iter.next();
-		if(!scannedNames.has(toScan.value)) {
-			scannedNames.add(toScan.value);
-			ns.scan(toScan.value).forEach(i => {
-				if(scannedFromDict[i] === undefined) {
-					scannedFromDict[i] = toScan.value;
+	while (allNames.size > scannedNamesSet.size) {
+		/** @type {string} */
+		var toScan = iter.next().value;
+		if (!scannedNamesSet.has(toScan)) {
+			scannedNamesSet.add(toScan);
+			ns.scan(toScan).forEach(i => {
+				if (!scannedFromMap.has(i)) {
+					scannedFromMap.set(i, toScan);
 				}
 				allNames.add(i);
 			});
 		}
 	}
-
-	// ADAM. What are you doing with types here? I'm sorry, me.
-	scannedNames = [...scannedNames.values()];
-	if ([...scannedNames].indexOf(ns.args[0]) === -1) {
-		ns.tprint("Possible servers:");
-		if(ns.args[0]) {
-			var matches = scannedNames.filter(v => v.startsWith(ns.args[0]));
-			if(matches.length === 1) {
-				ns.args[0] = matches[0];
-			}
-			else {
-				ns.tprint(scannedNames.filter(v => v.startsWith(ns.args[0])).join(", "));
-				return;
-			}
-		}
-		else {
-			ns.tprint([...scannedNames].filter(host => !host.startsWith("AUTO-")).sort().join(", "));
-			return;
-		}
-	}
-
-	//ns.tprint(JSON.stringify(scannedFromDict));
-	
-	//ns.tprint(Object.keys(scannedFromDict).map((host => "home;" + connectStr(scannedFromDict, host) + "backdoor;\n")).join(""));
-	connectArr(scannedFromDict, ns.args[0]).forEach((host) => {
-		ns.connect(host);
-	});
+	return {
+		scannedNames: [...scannedNamesSet.values()],
+		scannedFromDict: scannedFromMap
+	};
 }
 
-function connectArr(scannedFromDict, dest) {
+/**
+ * 
+ * @param {Map<string,string>} scannedFromDict
+ * @param {string} dest
+ * @returns {string[]}
+ */
+function getPathToServer(scannedFromDict, dest) {
 	var stack = [dest];
 
-	while(stack[0] !== "home") {
-		stack.unshift(scannedFromDict[stack[0]]);
+	while (stack[0] !== "home") {
+		stack.unshift(scannedFromDict.get(stack[0]));
 	}
 
 	return stack;
